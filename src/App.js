24f86a8e41df14d1a1272768a21e5cf02eb0a30b
Jatin Gambhir <jatin.gambhir@jungleworks.com>
2023-04-30 19:33:00 +0530

    Added Dark/Light mode switch

--- src/App.js
+++ src/App.js
@@ -7,39 +7,66 @@ import UserDetails from './components/UserDetails';
 function App() {
 
   const { REACT_APP_GITHUB_BASE_URL } = process.env;
+  const [isDarkMode, setIsDarkMode] = useState(false);
   const [user, setUser] = useState({});
   const [repo, setRepo] = useState([]);
 
   const userNameInputRef = useRef();
 
-  async function searchHandler() {
-    const enteredUserName = userNameInputRef.current.value;
+  const searchHandler = async () => {
+    const userName = userNameInputRef.current.value;
 
-    let user_details = await fetch(`${REACT_APP_GITHUB_BASE_URL}/users/${enteredUserName}`);
+    const user = await getUser(userName);
+    const repo = await getRepo(userName);
+
+    setUser(user);
+    setRepo(repo);
+  };
+
+  const getUser = async (userName) => {
+    let user_details = await fetch(`${REACT_APP_GITHUB_BASE_URL}/users/${userName}`);
     user_details = await user_details.json();
+    return user_details;
+  };
 
-    setUser(user_details);
-
-    let repo_details = await fetch(`${REACT_APP_GITHUB_BASE_URL}/users/${enteredUserName}/repos`);
+  const getRepo = async (userName) => {
+    let repo_details = await fetch(`${REACT_APP_GITHUB_BASE_URL}/users/${userName}/repos`);
     repo_details = await repo_details.json();
+    return repo_details;
+  };
 
-    setRepo(repo_details);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
-  }
 
   useEffect(() => {
 
   }, [user, repo]);
 
   return (
-    <div className="App">
+    <div className={`App ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
       <div className='container'>
-        <h1 className='text-center mt-4'>Search for Github User</h1>
-        <div className="form-group">
-          <input type="email" className="form-control" placeholder="Username" ref={userNameInputRef} width='75%'/>
+        <div className='search-container'>
+          <h1 className='text-center mt-4'>Search for Github User</h1>
+          <div className='toggle-container'>
+            <div className='toggle'>
+              <input
+                checked={isDarkMode}
+                onChange={toggleDarkMode}
+                id='switch'
+                className='switch'
+                type='checkbox'
+              />
+              <label htmlFor='switch'></label>
+            </div>
+          </div>
         </div>
-        <Button block type="primary" shape="round" className='btn'  onClick={searchHandler}>Search User</Button>
-        <div className='mt-4'>
+        <div className="form-group">
+          <input type="email" className="form-control" placeholder="Username" ref={userNameInputRef} width='75%' />
+        </div>
+        <Button block type="primary" shape="round" className='btn' onClick={searchHandler}>Search User</Button>
+        <div className='mt-4 mb-4'>
           <UserDetails user_details={user} repo_details={repo} />
         </div>
       </div>
